74796d7e33642f48491a8129ecb6493910939559
Margo <margop@Margos-MacBook-Air.local>
2019-05-16 18:04:12 -0500

    completed requirements up to Case 2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ db = SQLAlchemy(app)
 
 class Blog(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(200))
     
@@ -21,11 +21,15 @@ class Blog(db.Model):
 
 @app.route('/blog', methods=['POST', 'GET'])
 def index():
-    
-    blogs = Blog.query.filter_by().all()
-    print(len(blogs))
-    return render_template('listings.html', name = 'Home Blog',
+    id = request.args.get("id")
+    if not id:
+        blogs = Blog.query.filter_by().all()
+        return render_template('listings.html', name = 'Home Blog',
      blogs = blogs)
+    else:
+        blog = Blog.query.get(id)
+        return render_template('blog-post.html', blog = blog)
+    
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def new_post():
@@ -33,13 +37,17 @@ def new_post():
     if request.method == 'POST':
         blog_title = request.form["title"]
         blog_body =  request.form["body"]
+        blog_id = request.form['id']
         new_blog=Blog(blog_title, blog_body)
         db.session.add(new_blog)
         db.session.commit()
-
-        return redirect('/blog')
+        # TODO - create a method to pass new entry id into redirect
+        a = new_blog.id
+        
+        return redirect("./blog?id={{a}}")
     else:
         return render_template('new-post.html', name = 'New Post')
+
 
 if __name__ == '__main__':
     app.run()
